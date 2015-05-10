c29fe961c088a9c8de9d765036d8b48c0f15f6a8
gnishida <gnishida@purdue.edu>
2015-05-10 18:39:15 -0400

    update document

--- PCA/main.cpp
+++ PCA/main.cpp
@@ -8,14 +8,16 @@ int main() {
 	// 1 2 3
 	// 3 1 0
 	// 2 1 4
-	// 1 5 -1
-	cv::Mat_<double> X = (cv::Mat_<double>(4, 3) << 1,2,3,3,1,0,2,1,4,1,5,-1);
+	cv::Mat_<double> X = (cv::Mat_<double>(3, 3) << 1,2,3,3,1,0,2,1,4);
 
+	// 各行が、各データ
 	cv::PCA pca(X, cv::Mat(), CV_PCA_DATA_AS_ROW);
 	cout << pca.eigenvalues << endl;
 
 	// 固有ベクトル（各行が各固有ベクトル）
 	cout << pca.eigenvectors << endl;
+
+	cout << pca.project(X) << endl;
 
 	return 0;
 }
